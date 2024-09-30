be3131cce51304c041c42732a6b31f27c9aeb2f7
orligortler <orlisqueak@gmail.com>
2024-09-30 10:37:19 -0400

    progress #1

--- src/App.java
+++ src/App.java
@@ -1,20 +1,55 @@
 import processing.core.*;
 
-public class App extends PApplet{
-    public static void main(String[] args)  {
+public class App extends PApplet {
+    public static void main(String[] args) {
         PApplet.main("App");
+
     }
 
-    public void setup(){
-        
+    int u = 0;
+    int car = 3;
+    int x = 300;
+    int y = 50;
+    int speed = 9;
+    
+
+    public void setup() {
+        background(200, 90, 100);
+
     }
 
-    public void settings(){
-        
+    public void settings() {
+        size(600, 600);
     }
 
-    public void draw(){
-       
+    public void draw() {
+        background(200, 90, 100);
+        fill(200, 200);
+        rect(u, height / 3 , 140, 70);
 
+        u += car;
+        if (u > width) {
+            u = -5;
+        }
+        fill(255, 255, 0);
+        ellipse(x, y, 50, 50);
+
+        float distance = dist(x, y, u, height / 3);
+        fill(0);
+        textSize(16);
+        text("Distance: " + nf(distance, 0, 2), 10, height - 10);
+        }
+    
+
+    public void keyPressed() {
+        if (keyCode == LEFT) {
+          x -= speed;
+        } else if (keyCode == RIGHT) {
+          x += speed;
+        } else if (keyCode == UP) {
+          y -= speed;
+        } else if (keyCode == DOWN) {
+          y += speed;
+        }
     }
 }
